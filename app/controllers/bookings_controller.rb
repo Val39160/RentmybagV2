dfa27f2f8fe379bd6fcb3df1a3f360cd5432a406
asya hambrick <asya.simone@orange.fr>
2024-07-09 15:03:40 +0200

    create bookings and redirect to show is complete

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,19 @@
 class BookingsController < ApplicationController
-end
+  def create
+    @booking = Booking.new(booking_params)
+    @user = current_user
+    @booking.user = @user
+    @bag = Bag.find(params[:id])
+    @booking.bag = @bag
+    if @booking.save!
+      redirect_to dashboard_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+    private
+
+    def booking_params
+      params.require(:booking).permit(:user_id, :bag_id, :start_date, :end_date, :total_price, :status)
+    end
+  end
