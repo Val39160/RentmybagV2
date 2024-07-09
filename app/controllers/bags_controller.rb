104f1431c68fc8ba80ad65edf0167e6dd2282514
valentin corger <valentin.corger@gmail.com>
2024-07-09 15:03:33 +0200

    done the create method on bags controller

--- app/controllers/bags_controller.rb
+++ app/controllers/bags_controller.rb
@@ -1,2 +1,18 @@
 class BagsController < ApplicationController
+
+  def create
+    @bag = Bag.new(bag_params)
+    if @bag.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+
+  def bag_params
+    params.require(:bag).permit(:brand, :state, :capacity, :usage, :address, :availability, :daily_price)
+  end
 end
