401545567e605df3cbb795d533e9d5d289649ae5
valentin corger <valentin.corger@gmail.com>
2024-07-11 11:53:36 +0200

    done

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
   def profile
     @bags = Bag.where(user: current_user)
     @bookings = Booking.where(user: current_user)
