869771fe1f30916699a93d144fac1ddc5aaf9627
asya hambrick <asya.simone@orange.fr>
2024-07-11 16:36:32 +0200

    dashboard css complete

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,6 @@ class DashboardController < ApplicationController
   def profile
     @bags = Bag.where(user: current_user)
     @bookings = Booking.where(user: current_user)
+    @owner_bookings = Booking.joins(:bag).where(bags: { user: current_user })
   end
 end
